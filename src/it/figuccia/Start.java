1941ac8b490ae2f0c51a9ee4af0db3b3f258dcb2
Keicabba <peppino_cbr@hotmail.com>
2023-01-30 18:06:43 +0100

    aggiunto un codice semplificato alternartivo come commento

--- src/it/figuccia/Start.java
+++ src/it/figuccia/Start.java
@@ -6,7 +6,7 @@ public class Start {
         String phrase = "I Like to code near the Lake with my friend Luke";
 
         String result = phrase.replaceAll("(?i)(?<!friend\s)l[a-z]*ke", "BLA");
-
+//        String result = phrase.replaceAll("L[^u]ke", "BLA");
         System.out.println(result);
     }
 }
